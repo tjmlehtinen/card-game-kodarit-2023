3b130b6c32258eed34e05cd68abc4920ed897365
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2023-09-25 19:31:37 +0300

    nappi kirjoittaa nappia painettu

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,27 @@ const playerCard = {
           {name: "Speed", value: 20}]
 }
 
+const opponentCard = {
+  image: "http://placekitten.com/120/110",
+  stats: [{name: "Cuteness", value: 20},
+          {name: "Speed", value: 30}]
+}
+
 export default function App() {
+  function compareCards() {
+    console.log("nappia painettu")
+  }
   return (
     <>
       <h1>Korttipeli</h1>
       <div className="game">
         <Card card={playerCard}/>
+        <div>
+          <button type="button" onClick={compareCards}>
+            Play
+          </button>
+        </div>
+        <Card card={opponentCard}/>
       </div>
     </>
   );
