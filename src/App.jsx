91dd84bd39e385dcfecda326e9525327563ba2d2
Your Name <you@example.com>
2023-10-16 19:34:09 +0300

    random cards generated

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
-const playerCard = {
-  image: "http://placekitten.com/120/100",
-  stats: [{name: "Cuteness", value: 30},
-          {name: "Speed", value: 20}]
-}
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
 
-const opponentCard = {
-  image: "http://placekitten.com/120/110",
-  stats: [{name: "Cuteness", value: 20},
-          {name: "Speed", value: 30}]
-}
+const getRandomCard = () => {
+  return {
+  image: "http://placekitten.com/120/100?image=" + getRandomInt(1, 16),
+  stats: [{name: "Cuteness", value: getRandomInt(10, 30)},
+          {name: "Stealthmode", value: getRandomInt(10, 50)},
+          {name: "Speed", value: getRandomInt(10, 40)}]
+  }
+};
+
+const getCards = (n) => Array(n).fill(null).map((_) => getRandomCard());
+
+const playerCards = getCards(8);
+
+const opponentCards = getCards(8);
 
 export default function App() {
   const [result, setResult] = useState("");
@@ -27,14 +32,14 @@ export default function App() {
     <>
       <h1>Korttipeli</h1>
       <div className="game">
-        <Card card={playerCard}/>
+        {playerCards.map((c) => <Card card={c}/>)}
         <div>
           <p>{result || "Press play!"}</p>
           <button type="button" onClick={compareCards}>
             Play
           </button>
         </div>
-        <Card card={opponentCard}/>
+        {opponentCards.map((c) => <Card card={c}/>)}
       </div>
     </>
   );
