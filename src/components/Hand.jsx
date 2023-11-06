852358f8a3f68f7d2b27e4d425ffb3d71918e88f
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2023-11-06 19:29:49 +0200

    z-index

--- src/components/Hand.jsx
+++ src/components/Hand.jsx
@@ -3,8 +3,11 @@ import Card from "./Card"
 export default function Hand({ cards, who }) {
 return (
     <ul className={`hand-list ${who}`}>
-        {cards.map((c) => 
-        <li className={`hand-list-item ${who}`} key={c.id}>
+        {cards.map((c, index) => 
+        <li className={`hand-list-item ${who}`}
+            key={c.id}
+            style={{zIndex: cards.length - index}}
+        >
             <Card card={c}/>
         </li>
         )}
---
+++ src/components/PlayButton.jsx
@@ -0,0 +1,7 @@
+export default function PlayButton() {
+    return (
+        <button type="button">
+            Play
+        </button>
+    );
+}
